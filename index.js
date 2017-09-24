cce076ffa128b28430a234018d636133534e7059
Vlad Chernyshov <bloodcarter@gmail.com>
2017-09-24 15:23:27 +0700

    Fix level being overwritten in fixLevel

--- index.js
+++ index.js
@@ -24,8 +24,6 @@ function pinoMultiStream (opts, stream) {
   return fixLevel(pino(toPino, multistream({ stream: iopts.stream, level: iopts.level })))
 
   function fixLevel (pino) {
-    pino.levelVal = pino.stream.minLevel
-
     if (Array.isArray(iopts.streams)) {
       iopts.streams.forEach(function (s) {
         if (s.levelVal) {
